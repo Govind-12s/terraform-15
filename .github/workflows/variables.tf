0480d30cdc54f0de7af3ee57d83d05f62ab4adf8
Govind <gs5869121@gmail.com>
2025-07-03 15:14:22 +0530

    Update variables.tf

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,10 +1,10 @@
 variable "ami_id" {
-  description = "AMI ID"
+  description = "ami-0d03cb826412c6b0f"
   type        = string
 }
 
 variable "instance_type" {
-  description = "Instance type"
+  description = "t2.micro"
   type        = string
   default     = "t2.micro"
 }
